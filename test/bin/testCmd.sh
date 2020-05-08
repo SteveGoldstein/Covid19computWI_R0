d641914320bb183cfa9d0aa6e2d3546ad48191fe
Steve Goldstein <sgoldstein@wisc.edu>
2020-05-08 16:36:19 -0500

    wrote automated tests

---
+++ test/bin/testCmd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## 4 test cases:  Usage test/bin/testCmd.sh test/model| test/output
+dir=$1
+
+### defaults
+i=0;
+Rscript --vanilla bin/computeWI_R_0.R -outFile $dir/fromFile.$i.csv -inFile test/input/sw.csv  1> $dir/fromFile.$i.out 2> $dir/fromFile.$i.err &
+
+#### only current R0  
+i=1;
+Rscript --vanilla bin/computeWI_R_0.R -current TRUE -outFile $dir/fromFile.$i.csv -inFile test/input/sw.csv  1> $dir/fromFile.$i.out 2> $dir/fromFile.$i.err &
+
+#### exclude state  
+i=2;
+Rscript --vanilla bin/computeWI_R_0.R -includeState FALSE -outFile $dir/fromFile.$i.csv -inFile test/input/sw.csv  1> $dir/fromFile.$i.out 2> $dir/fromFile.$i.err &
+
+
+#### only current R0; exclude state  
+i=3;
+Rscript --vanilla bin/computeWI_R_0.R -includeState FALSE -current TRUE -outFile $dir/fromFile.$i.csv -inFile test/input/sw.csv  1> $dir/fromFile.$i.out 2> $dir/fromFile.$i.err &
+
