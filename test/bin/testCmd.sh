b1e22e5d172d03b41d1da87c1d32b1b0000b959b
Steve Goldstein <sgoldstein@wisc.edu>
2020-05-20 09:56:50 -0500

    changed names of test output files

--- test/bin/testCmd.sh
+++ test/bin/testCmd.sh
@@ -5,22 +5,22 @@ dir=$1
 
 ### defaults
 i=0;
-Rscript --vanilla bin/computeWI_R_0.R -outFile $dir/fromFile.$i.csv -inFile test/input/sw.csv  1> $dir/fromFile.$i.out 2> $dir/fromFile.$i.err &
+Rscript --vanilla bin/computeWI_R_0.R -outFile $dir/test.$i.csv -inFile test/input/sw.csv  1> $dir/test.$i.out 2> $dir/test.$i.err &
 
 #### only current R0  
 i=1;
-Rscript --vanilla bin/computeWI_R_0.R  -current TRUE -outFile $dir/fromFile.$i.csv -inFile test/input/sw.csv  1> $dir/fromFile.$i.out 2> $dir/fromFile.$i.err &
+Rscript --vanilla bin/computeWI_R_0.R  -current TRUE -outFile $dir/test.$i.csv -inFile test/input/sw.csv  1> $dir/test.$i.out 2> $dir/test.$i.err &
 
 #### exclude state  
 i=2;
-Rscript --vanilla bin/computeWI_R_0.R -aggregateLabel none -outFile $dir/fromFile.$i.csv -inFile test/input/sw.csv  1> $dir/fromFile.$i.out 2> $dir/fromFile.$i.err &
+Rscript --vanilla bin/computeWI_R_0.R -aggregateLabel none -outFile $dir/test.$i.csv -inFile test/input/sw.csv  1> $dir/test.$i.out 2> $dir/test.$i.err &
 
 
 #### only current R0; exclude state  
 i=3;
-Rscript --vanilla bin/computeWI_R_0.R -aggregateLabel none -current TRUE -outFile $dir/fromFile.$i.csv -inFile test/input/sw.csv  1> $dir/fromFile.$i.out 2> $dir/fromFile.$i.err &
+Rscript --vanilla bin/computeWI_R_0.R -aggregateLabel none -current TRUE -outFile $dir/test.$i.csv -inFile test/input/sw.csv  1> $dir/test.$i.out 2> $dir/test.$i.err &
 
 #### only current R0; include state;  -verbose and -plotFile 
 i=4;
-Rscript --vanilla bin/computeWI_R_0.R -aggregateLabel Wisconsin -current TRUE -outFile $dir/fromFile.$i.csv -inFile test/input/sw.csv  -verbose TRUE -plotFile $dir/fromFile.$i.pdf 1> $dir/fromFile.$i.out 2> $dir/fromFile.$i.err &
+Rscript --vanilla bin/computeWI_R_0.R -aggregateLabel Wisconsin -current TRUE -outFile $dir/test.$i.csv -inFile test/input/sw.csv  -verbose TRUE -plotFile $dir/test.$i.pdf 1> $dir/test.$i.out 2> $dir/test.$i.err &
 
